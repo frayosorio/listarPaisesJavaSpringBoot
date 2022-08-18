31dd1726d3a180592c780f2ae9e7e7f0e4bed310
Fray León Osorio Rivera <frayosorio@gmail.com>
2022-08-17 21:30:40 -0500

    inicio del proyecto

---
+++ src/main/java/listarciudades/listarciudades/ListarciudadesApplication.java
@@ -0,0 +1,22 @@
+package listarciudades.listarciudades;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListarciudadesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListarciudadesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.print("Feliz día del Ingeniero!");
+		
+	}
+
+
+
+}
---
+++ src/test/java/listarciudades/listarciudades/ListarciudadesApplicationTests.java
@@ -0,0 +1,13 @@
+package listarciudades.listarciudades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListarciudadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
